9ef1b074e1b16812549f552a10805b03befc8b90
RalphM-02 <ralph_marquez@dlsu.edu.ph>
2024-03-30 19:26:01 +0800

    Home Screen Frontend

--- app/src/main/java/com/mobdeve/s12/marquezgavanmaloles/mco/HomeScreen.kt
+++ app/src/main/java/com/mobdeve/s12/marquezgavanmaloles/mco/HomeScreen.kt
@@ -3,29 +3,39 @@ package com.mobdeve.s12.marquezgavanmaloles.mco
 import android.annotation.SuppressLint
 import android.content.res.Resources.Theme
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -38,7 +48,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mobdeve.s12.marquezgavanmaloles.mco.ui.theme.darkGray
 import com.mobdeve.s12.marquezgavanmaloles.mco.ui.theme.lightGreen
+import com.mobdeve.s12.marquezgavanmaloles.mco.ui.theme.midtoneGray
 
 data class BottomNavigationItem(
     val title: String,
@@ -46,71 +58,42 @@ data class BottomNavigationItem(
     val unselectedIcon: ImageVector,
 )
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(){
-    val items = listOf(
-        BottomNavigationItem(
-            title = "Home",
-            selectedIcon = Icons.Filled.Home,
-            unselectedIcon = Icons.Outlined.Home,
-        ),
-        BottomNavigationItem(
-            title = "Home",
-            selectedIcon = Icons.Filled.CheckCircle,
-            unselectedIcon = Icons.Outlined.CheckCircle,
-        ),
-        BottomNavigationItem(
-            title = "Home",
-            selectedIcon = Icons.Filled.Settings,
-            unselectedIcon = Icons.Outlined.Settings,
-        )
-    )
-    var selectedItemIndex by rememberSaveable {
-        mutableIntStateOf(0)
-    }
-    Scaffold (
-        bottomBar = {
-            NavigationBar {
-                items.forEachIndexed{ index, item ->
-                    NavigationBarItem(
-                        selected = selectedItemIndex == index,
-                        onClick = {
-                            selectedItemIndex = index
-                            // TODO: Navigate to page
-                        },
-                        icon = {
-                            Icon(
-                                imageVector = if (index == selectedItemIndex){
-                                    item.selectedIcon
-                                }else item.unselectedIcon,
-                                contentDescription = item.title
-                            )
-                        })
-                }
-            }
-        }
-    ){
-        Row(modifier = Modifier
+    Column(
+        modifier = Modifier
             .fillMaxWidth()
-            .height(100.dp)
-            .background(lightGreen),
-            horizontalArrangement = Arrangement.Center
-        ){
-
+            .height(650.dp)
+            .background(darkGray)
+            ,
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+    ) {
+        ExtendedFloatingActionButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .padding(20.dp)
+                .width(400.dp)
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Icon(Icons.Filled.Add, contentDescription = "Add")
+            Text(text = "Add a Task")
         }
     }
 }
 
+
 @Composable
-fun Task(){
-    // TODO: Task Item Layout 
+fun TaskCard(){
+    // TODO: Task Item Layout
 }
 
 @Preview
 @Composable
 fun HomePreview(){
-    Surface {
+    Surface (
+    ){
         Home()
     }
 }
--- app/src/main/java/com/mobdeve/s12/marquezgavanmaloles/mco/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s12/marquezgavanmaloles/mco/MainActivity.kt
@@ -1,28 +1,58 @@
 package com.mobdeve.s12.marquezgavanmaloles.mco
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.mobdeve.s12.marquezgavanmaloles.mco.ui.theme.MCOTheme
+import com.mobdeve.s12.marquezgavanmaloles.mco.ui.theme.lightGreen
+import com.mobdeve.s12.marquezgavanmaloles.mco.ui.theme.midtoneGray
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -32,7 +62,70 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Main()
+                    val items = listOf(
+                        BottomNavigationItem(
+                            title = "Home",
+                            selectedIcon = Icons.Filled.Home,
+                            unselectedIcon = Icons.Outlined.Home,
+                        ),
+                        BottomNavigationItem(
+                            title = "Rewards",
+                            selectedIcon = Icons.Filled.CheckCircle,
+                            unselectedIcon = Icons.Outlined.CheckCircle,
+                        ),
+                        BottomNavigationItem(
+                            title = "Profile",
+                            selectedIcon = Icons.Filled.AccountCircle,
+                            unselectedIcon = Icons.Outlined.AccountCircle,
+                        ),
+                        BottomNavigationItem(
+                            title = "Settings",
+                            selectedIcon = Icons.Filled.Settings,
+                            unselectedIcon = Icons.Outlined.Settings,
+                        )
+                    )
+                    var selectedItemIndex by rememberSaveable {
+                        mutableIntStateOf(0)
+                    }
+                    Scaffold (
+                        bottomBar = {
+                            NavigationBar {
+                                items.forEachIndexed{ index, item ->
+                                    NavigationBarItem(
+                                        selected = selectedItemIndex == index,
+                                        onClick = {
+                                            selectedItemIndex = index
+                                            // TODO: Navigate to page
+                                        },
+                                        icon = {
+                                            Icon(
+                                                imageVector = if (index == selectedItemIndex){
+                                                    item.selectedIcon
+                                                }else item.unselectedIcon,
+                                                contentDescription = item.title
+                                            )
+                                        })
+                                }
+                            }
+                        }
+                    ){
+                        Column {
+                            Row(modifier = Modifier
+                                .fillMaxWidth()
+                                .height(100.dp)
+                                .background(lightGreen),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center
+                            ){
+                                Text(text = "HocusFocus", style = MaterialTheme.typography.titleLarge)
+                            }
+                            Row(modifier = Modifier
+                                .fillMaxWidth()
+                            ){
+                                Main()
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -48,12 +141,4 @@ fun Main(){
         composable(Routes.HOME_SCREEN) { Home( /* ... */ ) }
     }
 
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MCOTheme {
-        Main()
-    }
 }
